6e61cf5dac14102f4cca94c4572824badec05c0d
Karan Shah <karanshah0206@outlook.com>
2020-05-05 13:21:32 +0530

    Video Emits on Socket

--- public/app.js
+++ public/app.js
@@ -13,12 +13,14 @@ context.height = canvas.height;
 // Video Functions
 function viewVideo (video, context) {
     context.drawImage(video, 0, 0, context.width, context.height);
+    socket.emit('stream', canvas.toDataURL('image/webp'));
 }
 function init () {
     navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitgetUserMedia || navigator.mozgetUserMedia || navigator.msggetUserMedia);
     if (navigator.getUserMedia) {
         navigator.getUserMedia({video: true}, loadPass, loadFail)
     }
+    setInterval(function(){viewVideo(video,context)},70);
 }
 function loadPass (stream) {
     video.srcObject = stream;

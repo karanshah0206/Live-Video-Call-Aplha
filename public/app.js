2546646a77661f7ca17c09a4386876d8cace735e
Karan Shah <karanshah0206@outlook.com>
2020-05-05 13:38:28 +0530

    Added Socket Receiver

--- public/app.js
+++ public/app.js
@@ -11,20 +11,26 @@ context.width = canvas.width;
 context.height = canvas.height;
 
 // Video Functions
-function viewVideo (video, context) {
-    context.drawImage(video, 0, 0, context.width, context.height);
-    socket.emit('stream', canvas.toDataURL('image/webp'));
-}
 function init () {
     navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitgetUserMedia || navigator.mozgetUserMedia || navigator.msggetUserMedia);
     if (navigator.getUserMedia) {
         navigator.getUserMedia({video: true}, loadPass, loadFail)
     }
-    setInterval(function(){viewVideo(video,context)},70);
+    setInterval(function(){viewVideo(video,context)},30);
 }
 function loadPass (stream) {
     video.srcObject = stream;
 }
 function loadFail () {
-    alert("Video Transmission Failed");
+    alert('Video Transmission Failed');
 }
+function viewVideo (video, context) {
+    context.drawImage(video, 0, 0, context.width, context.height);
+    socket.emit('stream', canvas.toDataURL('image/webp'));
+}
+
+// Socket Listeners
+socket.on('stream', (image) => {
+    var img = document.getElementById('play');
+    img.src = image;
+})

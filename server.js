2d2e6cf52d83a90093729115f0890e21ba5cc603
Karan Shah <karanshah0206@outlook.com>
2020-05-05 12:36:35 +0530

    Server.js Init

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+var port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => {
+    console.log('Listening on port: ' + port);
+})
